79951bf30e33f22989dff1989244ba165f2744a5
oli-mac <adumulugeta21@gmail.com>
2022-03-28 21:25:03 +0300

    delete tasks

--- base/views.py
+++ base/views.py
@@ -4,6 +4,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
 
 from django.urls import reverse_lazy
 
@@ -33,4 +35,8 @@ class TaskUpdate(UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-    
+class TaskDelete(DeleteView):
+
+    model = Task
+    context_object_name='task'
+    success_url = reverse_lazy('tasks')
