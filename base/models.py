e8f66e372ae8af3a3f7ee8ce855006a125ea8bc2
oli-mac <adumulugeta21@gmail.com>
2022-03-28 16:26:27 +0300

    first commit

---
+++ base/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models. CharField(max_length=200,null=True, blank=True )
+    disc = models.TextField(null=True, blank=True )
+    complete = models.BooleanField(default=False)
+    create = models.DateTimeField(auto_now_add=True)
