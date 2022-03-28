e5e49aa072fce237f9de8ab23dfea306ac9c523c
oli-mac <adumulugeta21@gmail.com>
2022-03-28 23:52:37 +0300

    login with validation of diff user

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from re import template
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -30,6 +31,13 @@ class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name='tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        return context
+
 class TaskDetail(LoginRequiredMixin,DetailView):
 
     model = Task
@@ -39,13 +47,17 @@ class TaskDetail(LoginRequiredMixin,DetailView):
 class TaskCreate(LoginRequiredMixin,CreateView):
 
     model = Task
-    fields = '__all__'
+    fields = ['title','disc','complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin,UpdateView):
 
     model = Task
-    fields = '__all__'
+    fields = ['title','disc','complete']
     success_url = reverse_lazy('tasks')
 
 class TaskDelete(LoginRequiredMixin,DeleteView):
