e0b268c1f682f8d698c0613b879049233da4157c
oli-mac <adumulugeta21@gmail.com>
2022-03-28 17:32:49 +0300

    detailview

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from .views import TaskList,TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 
 ]
